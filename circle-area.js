f04da2a2247a23f1917c48ff11252b94cb878ba1
Dimo D. Dimitrov <dimo_3@abv.bg>
2014-11-12 17:12:04 +0200

    Create circle-area.js

---
+++ circle-area.js
@@ -0,0 +1,10 @@
+//This is circle area task js file//
+
+function calcCircleArea(r) {
+    var r;
+    var area=Math.PI*r*r;
+    return area;
+}
+document.writeln(" r = 7 area = "+calcCircleArea(7)+"<br/>");
+document.writeln(" r = 1.5 area = "+calcCircleArea(1.5)+"<br/>");
+document.writeln(" r = 20 area = "+calcCircleArea(20)+"<br/>");
